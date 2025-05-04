ab496b27d3f44ee6f810d8efcdbf45484ee77d59
sachin-paranjape <sachinparanjape17@gmail.com>
2025-05-04 18:28:35 +0530

    Added built-in cd and execvp support

--- my_shell.c
+++ my_shell.c
@@ -6,34 +6,68 @@
 #include<sys/wait.h>
 
 #define MAX_CMD_LEN 1024
+#define MAX_ARGS 64
 
 int main()
 {
-    char command[MAX_CMD_LEN];
+    char input[MAX_CMD_LEN];
 
     while(1){
         printf("> ");
         fflush(stdout);
 
-        if(!fgets(command,MAX_CMD_LEN,stdin)){
+        if(!fgets(input,MAX_CMD_LEN,stdin)){
             break;
         }
 
-        command[strcspn(command,"\n")]=0;
+        //Remove newline
+        input[strcspn(input,"\n")]=0;
 
-        if(strcmp(command,"exit")==0){
-            break;
-        }
+        //Skip empty input
+        if(strlen(input)==0)
+            continue;
         
-        pid_t pid = fork();
-        if(pid == 0){
-            //Child process
-            execlp(command,command,(char*)NULL);
-            perror("execlp failed");
+        //Tokenize input into args[]
+        char* args[MAX_ARGS];
+        int arg_count=0;
+        char *token = strtok(input," ");
+
+        while(token!=NULL && arg_count < MAX_ARGS-1)
+        {
+            args[arg_count++]=token;
+            token= strtok(NULL," ");
+        }
+        args[arg_count]=NULL;
+
+        //Built-in command: exit
+        if(strcmp(args[0],"exit")==0){
+            break;
+        }
+
+        //Built in cd command
+        if(strcmp(args[0],"cd")==0){
+            if(args[1]==NULL){
+                fprintf(stderr,"cd:expected argument\n");
+            }
+            else{
+                if(chdir(args[1])!=0){
+                    perror("cd");
+                }
+            }
+            continue;
+        }
+
+        //Fork and exec for external commands
+        pid_t pid=fork();
+
+        if(pid==0){
+            execvp(args[0],args);
+            perror("execvp failed");
             exit(1);
-        }else{
-            //Parent process
+        }else if(pid > 0){
             wait(NULL);
+        }else{
+            perror("fork failed");
         }
     }
 
