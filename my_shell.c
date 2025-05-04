d434ad4a13267e4349ff7c186e6dab04a264c8a6
sachin-paranjape <sachinparanjape17@gmail.com>
2025-05-04 19:53:12 +0530

    Ignore compiled binary

--- my_shell.c
+++ my_shell.c
@@ -4,35 +4,31 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
 
-#define MAX_CMD_LEN 1024
 #define MAX_ARGS 64
 
 int main()
 {
-    char input[MAX_CMD_LEN];
-
     while(1){
-        printf("> ");
-        fflush(stdout);
-
-        if(!fgets(input,MAX_CMD_LEN,stdin)){
+        char* input = readline("> ");
+        if(!input){
             break;
         }
-
-        //Remove newline
-        input[strcspn(input,"\n")]=0;
-
-        //Skip empty input
-        if(strlen(input)==0)
+        if(strlen(input)==0){
+            free(input);
             continue;
-        
-        //Tokenize input into args[]
+        }
+
+        add_history(input);
+        /*Save input to history for arrow keys*/
+
+        //Parse input into arguments
         char* args[MAX_ARGS];
         int arg_count=0;
         char *token = strtok(input," ");
-
-        while(token!=NULL && arg_count < MAX_ARGS-1)
+        while(token && arg_count < MAX_ARGS-1)
         {
             args[arg_count++]=token;
             token= strtok(NULL," ");
@@ -41,35 +37,36 @@ int main()
 
         //Built-in command: exit
         if(strcmp(args[0],"exit")==0){
+            free(input);
             break;
         }
 
         //Built in cd command
         if(strcmp(args[0],"cd")==0){
-            if(args[1]==NULL){
-                fprintf(stderr,"cd:expected argument\n");
-            }
-            else{
+            if(args[1]){
                 if(chdir(args[1])!=0){
                     perror("cd");
                 }
+            }else{
+                fprintf(stderr,"cd: expected argument\n");
             }
+            free(input);
             continue;
         }
 
-        //Fork and exec for external commands
+        //External command
         pid_t pid=fork();
-
         if(pid==0){
             execvp(args[0],args);
             perror("execvp failed");
             exit(1);
-        }else if(pid > 0){
+        }else if(pid>0){
             wait(NULL);
         }else{
             perror("fork failed");
         }
-    }
 
+        free(input);
+    }
     return 0;
 }
